0c69c3114e539618b9dbe7a692302339580da6ce
t-e-r-m <newenewen@tutanota.com>
2021-04-11 00:05:40 +0530

    Properly configure mousekeys

--- corne/custom/config.h
+++ corne/custom/config.h
@@ -18,3 +18,26 @@
 /* Don't need these, copied them from another config. */
 /* #define TAPPING_FORCE_HOLD */
 /* #define TAPPING_TERM 100 */
+
+/* #define MOUSEKEY_INTERVAL 5 */
+/* #define MOUSEKEY_DELAY 0 */
+/* #define MOUSEKEY_TIME_TO_MAX 1 */
+/* #define MOUSEKEY_MAX_SPEED 10 */
+/* #define MOUSEKEY_WHEEL_DELAY 0 */
+/* #define MOUSEKEY_WHEEL_MAX_SPEED 12 */
+/* #define MOUSEKEY_WHEEL_TIME_TO_MAX 12 */
+/* #define MOUSEKEY_WHEEL_INTERVAL 20 */
+
+/* #define MK_KINETIC_SPEED */
+/* #define MOUSEKEY_DELAY 16 */
+/* #define MOUSEKEY_INTERVAL 16 */
+/* #define MOUSEKEY_MOVE_DELTA 50 */
+/* #define MOUSEKEY_INITIAL_SPEED 25 */
+/* #define MOUSEKEY_BASE_SPEED 200 */
+/* #define MOUSEKEY_DECELERATED_SPEED 100 */
+/* #define MOUSEKEY_ACCELERATED_SPEED 1000 */
+
+#define MK_3_SPEED
+#define MK_C_INTERVAL_UNMOD 8
+#define MK_C_OFFSET_UNMOD 20
+/* #define MK_W_INTERVAL_UNMOD 20 */
