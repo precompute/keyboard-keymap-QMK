a9e566e33bc7c9e925d5dc9d9e7c75cb28295485
t-e-r-m <newenewen@tutanota.com>
2021-04-17 22:18:14 +0530

    Add RGUI to map Linux's Hyper

--- corne/custom/keymap.c
+++ corne/custom/keymap.c
@@ -37,7 +37,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                            KC_H,
                            KC_COMM,
                            KC_DOT,
-                           KC_SLSH,
+                           RGUI_T(KC_SLSH),
                            LALT_T(KC_MINS),
 
 
