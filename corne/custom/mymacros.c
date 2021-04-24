802daebef0385dfb5c9091ec518846130df00d92
t-e-r-m <newenewen@tutanota.com>
2021-04-24 21:03:05 +0530

    Add vim edit, escape and window movement macros

--- corne/custom/mymacros.c
+++ corne/custom/mymacros.c
@@ -3,14 +3,55 @@
 
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
     switch (keycode) {
-    case QMKBEST:
-        if (record->event.pressed) {
-            // when keycode QMKBEST is pressed
-            SEND_STRING("QMK is the best thing ever!");
-        } else {
-            // when keycode QMKBEST is released
-        }
+    case VIMDOW_L:
+        if (record->event.pressed)
+            SEND_STRING(SS_LCTL("wh"));
+        return false;
+        break;
+    case VIMDOW_D:
+        if (record->event.pressed)
+            SEND_STRING(SS_LCTL("wj"));
+        return false;
+        break;
+    case VIMDOW_U:
+        if (record->event.pressed)
+            SEND_STRING(SS_LCTL("wk"));
+        return false;
+        break;
+    case VIMDOW_R:
+        if (record->event.pressed)
+            SEND_STRING(SS_LCTL("wl"));
+        return false;
+        break;
+    case VIMEDIT:
+        if (record->event.pressed)
+            SEND_STRING(SS_TAP(X_ESC)":e ~/");
+        return false;
+        break;
+    case VIMEXEC:
+        if (record->event.pressed)
+            SEND_STRING(SS_LALT("!"));
+        return false;
         break;
     }
     return true;
 };
+/* enum custom_keycoedes { */
+/*   CTLALTD = SAFE_RANGE, */
+/*   VIMSAVE, */
+/*   VIMWQ */
+/* }; */
+
+/* bool process_record_user(uint16_t keycode, keyrecord_t *record) { */
+/*   if(record->event.pressed) { */
+/*     switch(keycode) { */
+/*       case CTLALTD: */
+/*         SEND_STRING(SS_DOWN(X_LCTRL)SS_DOWN(X_LALT)SS_DOWN(X_DELETE)SS_UP(X_DELETE)SS_UP(X_LALT)SS_UP(X_LCTRL)); */
+/*         return false; */
+/*       case VIMSAVE: */
+/*         SEND_STRING(":w"); */
+/*         return false; */
+/*     } */
+/*   } */
+/*   return true; */
+/* }; */
--- corne/custom/mymacros.h
+++ corne/custom/mymacros.h
@@ -2,7 +2,12 @@
 #define MYMACROS_H
 
 enum custom_keycodes {
-    QMKBEST = SAFE_RANGE,
+    VIMDOW_L = SAFE_RANGE,
+    VIMDOW_D,
+    VIMDOW_U,
+    VIMDOW_R,
+    VIMEDIT,
+    VIMEXEC
 };
 
 #endif // MYMACROS_H
