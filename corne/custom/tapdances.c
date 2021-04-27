39b5f7d0e092948291128ba647be747e61a00b1b
t-e-r-m <newenewen@tutanota.com>
2021-04-28 00:46:58 +0530

    dsn't work yet, but the function is now legal
    although it is not clear if it will work via SRAM

--- corne/custom/tapdances.c
+++ corne/custom/tapdances.c
@@ -1,6 +1,10 @@
 /* -*- eval: (outshine-mode 1); -*- */
 #include QMK_KEYBOARD_H
 #include "tapdances.h"
+#include <stdint.h>
+
+#include "progmem.h"
+#include "send_string_keycodes.h"
 
 /* * State and Reset */
 // Determine the current tap dance state
@@ -24,6 +28,38 @@ void ql_reset(qk_tap_dance_state_t *state, void *user_data) {
 
 /* * Definitions */
 // Functions that control what our tap dance key does
+ /* //this is a PROGMEM-aware function in c */
+ /* printMsg_P(const char* str) { */
+ /*     char buf[strlen_P(str)+1]; */
+ /*     strcpy_P(buf, str); */
+ /*     puts(buf); */
+ /* } */
+
+/* void tapcode_SS(qk_tap_dance_state_t *state, void *user_data, uint16_t mykeycode, const char* mystring) { */
+void tapcode_SS(uint16_t mykeycode, const char* mystring, qk_tap_dance_state_t *state, void *user_data) {
+  ql_tap_state.state = cur_dance(state);
+  /* char myarr[] = *mystring; */
+  /* const char *myarr = mystring; */
+  /* const char *myarr = PSTR(mystring); */
+  char buf[strlen_P(mystring)+1];
+  strcpy_P(buf, mystring);
+  switch (ql_tap_state.state) {
+  case TD_SINGLE_TAP:
+    tap_code16(mykeycode);
+    break;
+  case TD_SINGLE_HOLD:
+    /* SEND_STRING(myarr); */
+    /* SEND_STRING(mystring); */
+    /* SEND_STRING(buf); */
+    send_string_P(buf);
+    /* send_string_P(PSTR(buf)); */
+    /* send_string_P(PSTR(mystring)); */
+    break;
+  case TD_NONE:
+    break;
+  }
+}
+
 /* ** LPRN  ) -> */
 void lprn_arrow_finished(qk_tap_dance_state_t *state, void *user_data) {
   ql_tap_state.state = cur_dance(state);
@@ -38,6 +74,7 @@ void lprn_arrow_finished(qk_tap_dance_state_t *state, void *user_data) {
     break;
   }
 }
+/* add a uint16_t keycode argument + a char* string argument to the function and use those in place of KC_LPRN and <-. */
 
 /* ** RPRN  ( <- */
 void rprn_arrow_finished(qk_tap_dance_state_t *state, void *user_data) {
@@ -57,6 +94,13 @@ void rprn_arrow_finished(qk_tap_dance_state_t *state, void *user_data) {
 /* * TD Enum */
 // Associate our tap dance key with its functionality
 qk_tap_dance_action_t tap_dance_actions[] = {
-  [LPRN_ARROW] = ACTION_TAP_DANCE_FN_ADVANCED_TIME(NULL, lprn_arrow_finished, ql_reset, 200),
-  [RPRN_ARROW] = ACTION_TAP_DANCE_FN_ADVANCED_TIME(NULL, rprn_arrow_finished, ql_reset, 200)
+  /* [LPRN_ARROW] = ACTION_TAP_DANCE_FN_ADVANCED_TIME(NULL, lprn_arrow_finished, ql_reset, 120), */
+  [LPRN_ARROW] = ACTION_TAP_DANCE_FN_ADVANCED_TIME(NULL, tapcode_SS(KC_LPRN, "<-"), ql_reset, 120),
+  [RPRN_ARROW] = ACTION_TAP_DANCE_FN_ADVANCED_TIME(NULL, rprn_arrow_finished, ql_reset, 120)
 };
+
+/* :s :%s */
+/* :mk */
+/* menu C-S-z */
+/* : =: */
+/* C-A-c C-A-y */
