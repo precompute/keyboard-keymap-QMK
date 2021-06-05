8c308018022201e6bc3334bd5ba119e7100d9d91
t-e-r-m <newenewen@tutanota.com>
2021-06-05 22:20:47 +0530

    Symmetric Hyper

--- corne/custom/keymap.c
+++ corne/custom/keymap.c
@@ -36,7 +36,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                            KC_Z,
                            KC_X,
                            KC_C,
-                           KC_D,
+                           RGUI_T(KC_D),
                            LT(5, KC_V),
                            KC_K,
                            KC_H,
