778e81e75b801e79e81da0cb598adbe7452eaece
t-e-r-m <newenewen@tutanota.com>
2022-12-11 13:47:28 +0530

    Move LALT back to KC_Z

--- corne/custom/keymap.c
+++ corne/custom/keymap.c
@@ -34,7 +34,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                          LGUI_T(KC_QUOT),
 
                          KC_DEL,
-                         KC_Z,
+                         LALT_T(KC_Z),
                          KC_X,
                          KC_C,
                          KC_D,
@@ -42,7 +42,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                          LT(9, KC_K),
                          KC_H,
                          KC_COMM,
-                         LALT_T(KC_DOT),
+                         KC_DOT,
                          RGUI_T(KC_SLSH),
                          LALT_T(KC_MINS),
 
